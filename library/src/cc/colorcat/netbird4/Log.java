ac811a31b4736c185dc61bfc9b02c6ec6ba7dc33
cxx <xx.ch@outlook.com>
2018-01-29 12:03:19 +0800

    developing

--- library/src/cc/colorcat/netbird4/Log.java
+++ library/src/cc/colorcat/netbird4/Log.java
@@ -7,8 +7,36 @@ package cc.colorcat.netbird4;
 final class Log {
     static Level threshold = Level.NOTHING;
 
-    static void e(Throwable throwable) {
+    static void v(String tag, String msg) {
+        log(tag, msg, Level.VERBOSE);
+    }
 
+    static void d(String tag, String msg) {
+        log(tag, msg, Level.DEBUG);
+    }
+
+    static void i(String tag, String msg) {
+        log(tag, msg, Level.INFO);
+    }
+
+    static void w(String tag, String msg) {
+        log(tag, msg, Level.WARN);
+    }
+
+    static void e(String tag, String msg) {
+        log(tag, msg, Level.ERROR);
+    }
+
+    static void e(Throwable throwable) {
+        if (Level.ERROR.priority >= threshold.priority) {
+            throwable.printStackTrace();
+        }
+    }
+
+    private static void log(String tag, String msg, Level level) {
+        if (level.priority >= threshold.priority) {
+            Platform.get().logger().log(tag, msg, level);
+        }
     }
 
     private Log() {
